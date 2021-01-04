e66745eb0f2c5ee35da3f5dbc52901a7dadfa475
Pythonology <pythonology@gmail.com>
2021-01-03 21:26:03 -0500

    Only apply text offset on first activation

--- Patches/MainMenuViewControllerPatch.cs
+++ Patches/MainMenuViewControllerPatch.cs
@@ -7,13 +7,16 @@ namespace BeatTogether.Patches
     [HarmonyPatch(typeof(MainMenuViewController), "DidActivate")]
     internal class DidActivatePatch
     {
-        internal static bool Prefix(MainMenuViewController __instance)
+        internal static bool Prefix(MainMenuViewController __instance, bool firstActivation)
         {
+            if (!firstActivation)
+                return true;
+
             var transform = __instance.gameObject.transform;
             var onlineButton = transform.Find("MainButtons/OnlineButton").gameObject;
             var onlineButtonTextObj = onlineButton.transform.Find("Text").gameObject;
             onlineButtonTextObj.transform.position = new Vector3(
-                0.43744f,
+                onlineButtonTextObj.transform.position.x + 0.025f,
                 onlineButtonTextObj.transform.position.y,
                 onlineButtonTextObj.transform.position.z
             );
--- Patches/MultiplayerModeSelectionViewControllerPatch.cs
+++ Patches/MultiplayerModeSelectionViewControllerPatch.cs
@@ -5,8 +5,11 @@ namespace BeatTogether.Patches
     [HarmonyPatch(typeof(MultiplayerModeSelectionViewController), "DidActivate")]
     internal class DidActivatePatch2
     {
-        internal static bool Prefix(MultiplayerModeSelectionViewController __instance)
+        internal static bool Prefix(MultiplayerModeSelectionViewController __instance, bool firstActivation)
         {
+            if (!firstActivation)
+                return true;
+
             var transform = __instance.gameObject.transform;
             var quickPlayButton = transform.Find("Buttons/QuickPlayButton").gameObject;
             quickPlayButton.SetActive(false);
