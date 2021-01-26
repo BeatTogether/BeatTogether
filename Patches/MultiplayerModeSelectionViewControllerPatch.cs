b50c08ca8ee2fa799f82405297ceccd2f00415e8
MT <littlehappytroll@gmail.com>
2021-01-26 10:30:48 -0500

    No blocking code execution

--- Patches/MultiplayerModeSelectionViewControllerPatch.cs
+++ Patches/MultiplayerModeSelectionViewControllerPatch.cs
@@ -5,15 +5,14 @@ namespace BeatTogether.Patches
     [HarmonyPatch(typeof(MultiplayerModeSelectionViewController), "DidActivate")]
     internal class DidActivatePatch2
     {
-        internal static bool Prefix(MultiplayerModeSelectionViewController __instance, bool firstActivation)
+        internal static void Prefix(MultiplayerModeSelectionViewController __instance, bool firstActivation)
         {
-            if (!Plugin.Configuration.Enabled || !firstActivation)
-                return true;
-
-            var transform = __instance.gameObject.transform;
-            var quickPlayButton = transform.Find("Buttons/QuickPlayButton").gameObject;
-            quickPlayButton.SetActive(false);
-            return true;
+            if (Plugin.Configuration.Enabled && firstActivation)
+            {
+                var transform = __instance.gameObject.transform;
+                var quickPlayButton = transform.Find("Buttons/QuickPlayButton").gameObject;
+                quickPlayButton.SetActive(false);
+            }
         }
     }
 }
--- Patches/NetworkConfigSOPatch.cs
+++ Patches/NetworkConfigSOPatch.cs
@@ -5,28 +5,26 @@ namespace BeatTogether.Patches
     [HarmonyPatch(typeof(NetworkConfigSO), "masterServerStatusUrl", MethodType.Getter)]
     internal class GetMasterServerStatusUrlPatch
     {
-        internal static bool Prefix(ref string __result)
+        internal static void Postfix(ref string __result)
         {
-            if (!Plugin.Configuration.Enabled)
-                return true;
-
-            __result = Plugin.Configuration.StatusUrl;
-            Plugin.Logger.Info($"Patching master server status URL (URL='{__result}').");
-            return false;
+            if (Plugin.Configuration.Enabled)
+            {
+                __result = Plugin.Configuration.StatusUrl;
+                Plugin.Logger.Info($"Patching master server status URL (URL='{__result}').");
+            }
         }
     }
 
     [HarmonyPatch(typeof(NetworkConfigSO), "masterServerEndPoint", MethodType.Getter)]
     internal class GetMasterServerEndPointPatch
     {
-        internal static bool Prefix(ref MasterServerEndPoint __result)
+        internal static void Postfix(ref MasterServerEndPoint __result)
         {
-            if (!Plugin.Configuration.Enabled)
-                return true;
-
-            __result = new MasterServerEndPoint(Plugin.Configuration.HostName, Plugin.Configuration.Port);
-            Plugin.Logger.Debug($"Patching master server end point (EndPoint='{__result}').");
-            return false;
+            if (Plugin.Configuration.Enabled)
+            {
+                __result = new MasterServerEndPoint(Plugin.Configuration.HostName, Plugin.Configuration.Port);
+                Plugin.Logger.Debug($"Patching master server end point (EndPoint='{__result}').");
+            }
         }
     }
 }
